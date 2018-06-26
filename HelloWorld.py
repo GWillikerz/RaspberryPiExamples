7d32862f10cfff1a7d93cc7de17db69d92ecbcdb
GWillikerz <billgerlib@gmail.com>
2018-06-26 03:50:35 +0000

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from time import sleep
+
+try:
+        while True
+            print "Heeeeeeey ya big ol World"
+            sleep(60)
+except KeyboardInterrupt, e:
+        logging.info("Stopping....")
+    
